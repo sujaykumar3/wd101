540dbd5291736e5cff59d905875f0a7427865f15
sujaykumar3 <134673407+sujaykumar3@users.noreply.github.com>
2023-11-21 22:01:14 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+
+    <script>
+        function validateUserRegistration() {
+            var alias = document.getElementById("userAlias").value;
+            var email = document.getElementById("userEmail").value;
+            var password = document.getElementById("userPassword").value;
+            var confirmPassword = document.getElementById("confirmUserPassword").value;
+            var errorMsg = document.getElementById("errorMsg");
+
+            errorMsg.innerHTML = "";
+
+            if (!alias || !email || !password || !confirmPassword) {
+                errorMsg.innerHTML = "All fields are required.";
+                return;
+            }
+
+            if (password !== confirmPassword) {
+                errorMsg.innerHTML = "Passwords do not match.";
+                return;
+            }
+
+            // Additional validation logic can be added here.
+
+            // If all validations pass, you can submit the form.
+            document.getElementById("userRegistrationForm").submit();
+        }
+    </script>
